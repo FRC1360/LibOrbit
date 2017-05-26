4b8990c0399a94c1617f9f64d1b9d4e963f495c1
Nicholas Mertin <nickmertin@gmail.com>
2017-05-26 11:09:56 -0400

    Cleaned up joystick base

--- src/ca/_1360/liborbit/io/OrbitJoystickBase.java
+++ src/ca/_1360/liborbit/io/OrbitJoystickBase.java
@@ -1,29 +1,9 @@
 package ca._1360.liborbit.io;
 
 public abstract class OrbitJoystickBase {
-    private OrbitJoystickProvider provider;
+    protected final OrbitJoystickProvider provider;
 
     public OrbitJoystickBase(OrbitJoystickProvider provider) {
         this.provider = provider;
-    }
-
-    protected final String getName() {
-        return provider.getName();
-    }
-
-    protected final double getAxis(int i) {
-        return provider.getAxis(i);
-    }
-
-    protected final boolean getButton(int i) {
-        return provider.getButton(i);
-    }
-
-    protected final int getPov(int i) {
-        return provider.getPov(i);
-    }
-
-    protected final void setOutput(int i, boolean v) {
-        provider.setOutput(i, v);
     }
 }
