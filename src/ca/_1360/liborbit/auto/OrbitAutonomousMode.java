4988b9904c8c75e13d2f2ba47a8f9eb74151e839
Nicholas Mertin <nickmertin@gmail.com>
2017-05-26 11:07:47 -0400

    Converted autonomous mode to an interface

--- src/ca/_1360/liborbit/auto/OrbitAutonomousMode.java
+++ src/ca/_1360/liborbit/auto/OrbitAutonomousMode.java
@@ -2,6 +2,6 @@ package ca._1360.liborbit.auto;
 
 import java.util.function.Consumer;
 
-public abstract class OrbitAutonomousMode<T> {
-    public abstract void add(Consumer<OrbitAutonomousCommand<T>> consumer);
+public interface OrbitAutonomousMode<T> {
+    void add(Consumer<OrbitAutonomousCommand<T>> consumer);
 }
