9740d0b1fe78db6e5950c8052a02ed6a11fe6dce
Nicholas Mertin <nickmertin@gmail.com>
2017-05-29 20:56:22 -0400

    Implemented motors, encoders, and solenoids

--- src/ca/_1360/liborbit/io/OrbitEncoder.java
+++ src/ca/_1360/liborbit/io/OrbitEncoder.java
@@ -1,25 +1,30 @@
 package ca._1360.liborbit.io;
 
-import ca._1360.liborbit.pipeline.OrbitPipelineInputEndpoint;
+import ca._1360.liborbit.pipeline.OrbitPipelineOutputEndpoint;
+import ca._1360.liborbit.pipeline.OrbitPipelineSimpleSource;
+
+import java.util.OptionalDouble;
 
 public final class OrbitEncoder {
-    private final OrbitPipelineInputEndpoint valueEndpoint;
-    private final OrbitPipelineInputEndpoint rateEndpoint;
+    private final OrbitEncoderProvider provider;
+    private final OrbitPipelineOutputEndpoint valueEndpoint;
+    private final OrbitPipelineOutputEndpoint rateEndpoint;
 
     public OrbitEncoder(int portA, int portB) {
-        valueEndpoint = null;
-        rateEndpoint = null;
+        provider = OrbitInputOutputManager.getProvider().getEncoder(portA, portB);
+        valueEndpoint = () -> OptionalDouble.of(provider.getPosition());
+        rateEndpoint = (OrbitPipelineSimpleSource) provider::getRate;
     }
 
     public int getValueInteger() {
         return 0;
     }
 
-    public OrbitPipelineInputEndpoint getValue() {
+    public OrbitPipelineOutputEndpoint getValue() {
         return valueEndpoint;
     }
 
-    public OrbitPipelineInputEndpoint getRate() {
+    public OrbitPipelineOutputEndpoint getRate() {
         return rateEndpoint;
     }
 }
