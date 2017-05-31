406944aaf502f465c8de2b35caef10fa4f1218b3
Nicholas Mertin <nickmertin@gmail.com>
2017-05-31 15:54:57 -0400

    Updated emulator test

--- src/ca/_1360/liborbit/test/EmulatorTest.java
+++ src/ca/_1360/liborbit/test/EmulatorTest.java
@@ -9,6 +9,7 @@ import ca._1360.liborbit.pipeline.OrbitPipelineConstantValueSource;
 import ca._1360.liborbit.pipeline.OrbitPipelineInvalidConfigurationException;
 import ca._1360.liborbit.pipeline.OrbitPipelineManager;
 import ca._1360.liborbit.pipeline.filters.OrbitSlewFilter;
+import ca._1360.liborbit.util.OrbitContainer;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
@@ -16,13 +17,15 @@ import java.util.concurrent.TimeUnit;
 public class EmulatorTest {
     public static void main(String[] args) throws OrbitPipelineInvalidConfigurationException, InterruptedException {
         OrbitEmulatedInputOutputProvider provider = new OrbitEmulatedInputOutputProvider();
-        provider.mapMotor(0, 0, 0.00884421747, 0.6);
+        OrbitContainer<Double> inertia = new OrbitContainer<>(0.00884421747);
+        OrbitContainer<Double> friction = new OrbitContainer<>(0.6);
+        provider.mapMotor(0, 0, inertia::getValue, friction::getValue);
         provider.mapEncoder(0, 1, 0, 14.0);
         OrbitInputOutputManager.setProvider(provider);
         OrbitMotor motor = new OrbitMotor(0, 0);
         OrbitEncoder encoder = new OrbitEncoder(0, 1);
         OrbitSlewFilter slew = new OrbitSlewFilter(0.01, -1.0, 1.0);
-        OrbitPipelineConstantValueSource targetPower = new OrbitPipelineConstantValueSource(0.8);
+        OrbitPipelineConstantValueSource targetPower = new OrbitPipelineConstantValueSource(1.0);
         new OrbitPipelineConnection(targetPower, slew, true);
         new OrbitPipelineConnection(slew, motor.getPowerEndpoint(), true);
         provider.start();
@@ -30,7 +33,10 @@ public class EmulatorTest {
             OrbitPipelineManager.updateAll();
             System.out.printf("%f,%f\n", encoder.getRate().get().orElse(0.0), motor.getCurrentEndpoint().get().orElse(0.0));
         }, 0, 10, TimeUnit.MILLISECONDS);
-        Thread.sleep(5000);
+        Thread.sleep(4000);
+        inertia.setValue(0.016);
+        friction.setValue(1.0);
+        Thread.sleep(1000);
         targetPower.setValue(-0.05);
         Thread.sleep(200);
         targetPower.setValue(0.0);
