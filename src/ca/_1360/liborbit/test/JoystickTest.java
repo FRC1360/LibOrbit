1fe5bd0f59d9ed22afb799e003595b91fc586208
Nicholas Mertin <nmertin@mertin.home>
2017-06-01 22:41:36 -0400

    Updated joystick test

--- src/ca/_1360/liborbit/test/JoystickTest.java
+++ src/ca/_1360/liborbit/test/JoystickTest.java
@@ -1,15 +1,26 @@
 package ca._1360.liborbit.test;
 
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
 import ca._1360.liborbit.io.OrbitEmulatedInputOutputProvider;
 import ca._1360.liborbit.io.OrbitInputOutputManager;
 import ca._1360.liborbit.io.OrbitLocalJoystickProvider;
+import ca._1360.liborbit.io.OrbitXbox360Controller;
+import ca._1360.liborbit.pipeline.OrbitPipelineConnection;
+import ca._1360.liborbit.pipeline.OrbitPipelineExportConnector;
+import ca._1360.liborbit.pipeline.OrbitPipelineInvalidConfigurationException;
+import ca._1360.liborbit.pipeline.OrbitPipelineManager;
 
 public final class JoystickTest {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws OrbitPipelineInvalidConfigurationException {
     	OrbitInputOutputManager.setProvider(new OrbitEmulatedInputOutputProvider(null));
-        int count = OrbitLocalJoystickProvider.refresh();
-        for (int i = 0; i < count; ++i) {
-        	System.out.println(OrbitInputOutputManager.getProvider().getJoystick(i).getName());
-        }
+        boolean[] connected = OrbitLocalJoystickProvider.refresh();
+        if (!connected[0])
+        	throw new RuntimeException();
+        OrbitXbox360Controller controller = new OrbitXbox360Controller(0);
+        new OrbitPipelineConnection(controller.getRightTrigger(), System.out::println, true);
+        new OrbitPipelineConnection(controller.getLeftTrigger(), new OrbitPipelineExportConnector<>(x -> x > 0.9, controller::vibrateLeft), true);
+        new ScheduledThreadPoolExecutor(1).scheduleAtFixedRate(OrbitPipelineManager::updateAll, 0, 100, TimeUnit.MILLISECONDS);
     }
 }
