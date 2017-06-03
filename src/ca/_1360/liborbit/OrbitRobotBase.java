c3951b6e611c315d31dbd5e62c14c631331b9992
Nicholas Mertin <nmertin@mertin.home>
2017-06-03 14:54:35 -0400

    Moved connect function to new robot utilities class

--- src/ca/_1360/liborbit/OrbitRobotBase.java
+++ src/ca/_1360/liborbit/OrbitRobotBase.java
@@ -6,25 +6,13 @@ import java.util.concurrent.TimeUnit;
 import com.kauailabs.navx.frc.AHRS;
 
 import ca._1360.liborbit.io.OrbitEmulatedInputOutputProvider;
-import ca._1360.liborbit.pipeline.OrbitPipelineConnection;
-import ca._1360.liborbit.pipeline.OrbitPipelineInputEndpoint;
-import ca._1360.liborbit.pipeline.OrbitPipelineInvalidConfigurationException;
 import ca._1360.liborbit.pipeline.OrbitPipelineManager;
-import ca._1360.liborbit.pipeline.OrbitPipelineOutputEndpoint;
 import ca._1360.liborbit.util.function.OrbitFunctionUtilities;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public abstract class OrbitRobotBase extends IterativeRobot {
 	public OrbitRobotBase(long cyclePeriod, TimeUnit cycleUnit) {
 		new ScheduledThreadPoolExecutor(1).scheduleAtFixedRate(OrbitFunctionUtilities.combine(OrbitPipelineManager::updateAll, this::runCycle), 0, cyclePeriod, cycleUnit);
-	}
-	
-	protected static void connect(OrbitPipelineOutputEndpoint source, OrbitPipelineInputEndpoint destination) {
-		try {
-			new OrbitPipelineConnection(source, destination, true);
-		} catch (OrbitPipelineInvalidConfigurationException e) {
-			throw new RuntimeException(e);
-		}
 	}
 
 	protected abstract AHRS setUpAhrs();
