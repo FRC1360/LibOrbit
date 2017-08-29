b024e34957132c9abdd74ff135168d4b3e569265
Nicholas Mertin <nmertin@mertin.home>
2017-08-29 18:51:49 -0400

    Created Makefile and function definition for JNI

---
+++ lib/liborbit-native/src/jni/ca__1360_liborbit_util_jni_OrbitInputEventsReader.cpp
@@ -0,0 +1,5 @@
+#include "ca__1360_liborbit_util_jni_OrbitInputEventsReader.h"
+
+JNIEXPORT void JNICALL Java_ca__11360_liborbit_util_jni_OrbitInputEventsReader_run (JNIEnv *env, jobject _this) {
+  //TODO: call main code for running read loop
+}
