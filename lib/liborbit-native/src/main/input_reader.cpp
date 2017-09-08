0b7371e812be8a6732e9132dcf9d1672b6417fdc
Nicholas Mertin <nmertin@mertin.home>
2017-09-08 14:32:22 -0400

    Implemented input_reader in liborbit-native

---
+++ lib/liborbit-native/src/main/input_reader.cpp
@@ -0,0 +1,41 @@
+#include "input_reader.h"
+#include <cstring>
+#include <sstream>
+
+static const char *make_command(const char *path) {
+    char *command = new char[strlen(path) + 6];
+    sprintf(command, "cat \"%s\"", path);
+    return command;
+}
+
+namespace liborbit {
+    input_reader::input_reader(const char *command, std::function<void(input_event)> handler, std::function<void(std::string)> error_handler, void *) : process(command, [this] (int status) { handle_status(status); }, [this] (char *signal) { handle_signal(signal); }), handler(handler), error_handler(error_handler) {
+        delete[] command;
+    }
+
+    input_reader::input_reader(const char *path, std::function<void(input_event)> handler, std::function<void(std::string)> error_handler) : input_reader(make_command(path), handler, error_handler, nullptr) {}
+
+    void input_reader::handle_status(int status) {
+        if (status) {
+            std::ostringstream error;
+            error << "Status code: " << status;
+            error_handler(error.str());
+        }
+    }
+
+    void input_reader::handle_signal(const char *signal) {
+        std::ostringstream error;
+        error << "Signal: " << signal;
+        error_handler(error.str());
+    }
+
+    void input_reader::run_loop(std::function<void()> stop) {
+        this->stop = stop;
+    }
+
+    input_reader::~input_reader() {
+        if (stop) {
+            stop();
+        }
+    }
+}
---
+++ lib/liborbit-native/src/main/input_reader.h
@@ -0,0 +1,26 @@
+#include <functional>
+#include <string>
+#include <linux/input.h>
+#include "pseudo_sudo.h"
+
+namespace liborbit {
+    class input_reader final {
+    private:
+        pseudo_sudo_process process;
+        std::function<void(input_event)> handler;
+        std::function<void(std::string)> error_handler;
+        std::function<void()> stop;
+        
+        input_reader(const char *command, std::function<void(input_event)> handler, std::function<void(std::string)> error_handler, void *);
+
+        void handle_status(int code);
+        void handle_signal(const char *signal);
+
+    public:
+        input_reader(const char *path, std::function<void(input_event)> handler, std::function<void(std::string)> error_handler);
+
+        void run_loop(std::function<void()> stop);
+
+        ~input_reader();
+    };
+}
